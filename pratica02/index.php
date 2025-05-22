69ca56d3043887bdb36155cf105f7b8ba4aa4786
John E. Onias <119425650+JohnOnias@users.noreply.github.com>
2025-05-22 16:58:42 -0300

    init

---
+++ pratica02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDEX</title>
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+<div>
+<ol>
+    <li><a href="\pratica02\S01\01.PHP">SEÇÃO 01</a></li>
+    <li><a href="">SEÇÃO 02</a></li>
+    <li><a href="">SEÇÃO 03</a></li>
+    <li><a href="">SEÇÃO 04</a></li>
+    <li><a href="">SEÇÃO 05</a></li>
+    
+</ol>
+</div>
+
+</body>
+</html>
