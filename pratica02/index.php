aa10e209bb2ec109423ad2eb8c06f99782e8bd5e
John E. Onias <119425650+JohnOnias@users.noreply.github.com>
2025-05-28 14:55:16 -0300

    novas questoes adcionadas

---
+++ pratica02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDEX</title>
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+<div>
+<ol>
+    <li><a href="\pratica02\S01\01_ao_10.php">SEÇÃO 01</a></li>
+    <li><a href="\pratica02\S02\11_ao_20.php">SEÇÃO 02</a></li>
+    <li><a href="\pratica02\S03\21_ao_30.php">SEÇÃO 03</a></li>
+    <li><a href="">SEÇÃO 04</a></li>
+    <li><a href="">SEÇÃO 05</a></li>
+    
+</ol>
+</div>
+
+</body>
+</html>
